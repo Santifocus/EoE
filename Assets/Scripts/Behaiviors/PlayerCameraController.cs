538586dd8731dd4b9246fd935e75cc7ad7bf6031
Lucas <saculucas98@aol.com>
2019-10-21 17:45:38 +0200

    Physicall Camera Implemented

--- Assets/Scripts/Behaiviors/PlayerCameraController.cs
+++ Assets/Scripts/Behaiviors/PlayerCameraController.cs
@@ -31,6 +31,25 @@ namespace EoE
 		{
 			transform.position = Player.Instance.transform.position + playerSettigns.CameraAnchorOffset;
 			transform.eulerAngles = new Vector3(CurRotation.y, CurRotation.x, 0);
+
+			float camDist = GetCameraDistance();
+			if(camDist > playerCamera.transform.localPosition.z)
+				playerCamera.transform.localPosition = new Vector3(0, 0, camDist);
+			else
+				playerCamera.transform.localPosition = new Vector3(0, 0, Mathf.Lerp(playerCamera.transform.localPosition.z, camDist, Time.deltaTime * 3));
+		}
+
+		private float GetCameraDistance()
+		{
+			Vector3 rayDir = (playerCamera.transform.position - transform.position).normalized;
+			RaycastHit hit;
+
+			if(Physics.Raycast(transform.position, rayDir, out hit, playerSettigns.CameraToPlayerDistance, ConstantCollector.TERRAIN_LAYER))
+			{
+				return -(transform.position - hit.point).magnitude * 0.95f;
+			}
+			else
+				return -playerSettigns.CameraToPlayerDistance;
 		}
 
 		private void Update()
