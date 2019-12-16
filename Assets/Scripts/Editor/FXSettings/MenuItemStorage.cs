2919f293f79b2672de04d6637c1a4672bc1c1767
Lucas <saculucas98@aol.com>
2019-12-16 16:40:02 +0100

    Fixed SetDirty issue of ItemCollector

--- Assets/Scripts/Editor/FXSettings/MenuItemStorage.cs
+++ Assets/Scripts/Editor/FXSettings/MenuItemStorage.cs
@@ -69,6 +69,7 @@ namespace EoE.Information
 			{
 				ItemCollector itemCollector = (ItemCollector)AssetDatabase.LoadAssetAtPath(AssetDatabase.GUIDToAssetPath(itemCollectorGUID[0]), typeof(ItemCollector));
 				itemCollector.CollectData();
+				EditorUtility.SetDirty(itemCollector);
 			}
 		}
 	}
