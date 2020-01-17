ebc4198655b567832570099c1064e418f99bc541
Lucas <saculucas98@aol.com>
2020-01-17 19:49:56 +0100

    Base Logic added for MusicController

---
+++ Assets/Scripts/Behaiviors/MusicArea.cs
@@ -0,0 +1,31 @@
+﻿using EoE.Entities;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EoE.Sounds
+{
+	public class MusicArea : MonoBehaviour
+	{
+		[SerializeField] private float radius = 1;
+		[SerializeField] private int targetMusicIndex = 0;
+		private float radiusSQRT;
+
+		private void Start()
+		{
+			radiusSQRT = radius * radius;
+		}
+		private void Update()
+		{
+			if (Player.Existant)
+			{
+				float dist = (transform.position - Player.Instance.transform.position).sqrMagnitude;
+			}
+		}
+		private void OnDrawGizmosSelected()
+		{
+			Gizmos.color = Color.white;
+			Gizmos.DrawWireSphere(transform.position, radius);
+		}
+	}
+}
---
+++ Assets/Scripts/Behaiviors/MusicController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EoE.Sounds
+{
+	public class MusicController : MonoBehaviour
+	{
+		public static MusicController Instance { get; private set; }
+		private void Start()
+		{
+			Instance = this;
+		}
+	}
+}
