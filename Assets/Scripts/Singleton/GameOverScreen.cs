809a7fb4e67db1666c3a2b0d69e69c3df710a5d2
Lucas <saculucas98@aol.com>
2020-01-19 18:25:31 +0100

    Game Over Screen iteration

--- Assets/Scripts/Singleton/GameOverScreen.cs
+++ Assets/Scripts/Singleton/GameOverScreen.cs
@@ -10,29 +10,38 @@ namespace EoE.UI
 {
 	public class GameOverScreen : MonoBehaviour
 	{
+		[SerializeField] private int gameOverMusicIndex = 2;
+
 		[SerializeField] private Image blackPlane = default;
 		[SerializeField] private float finalBlackAlpha = 0.9f;
+		[SerializeField] private float blackPlaneFadeInTime = 2;
+
 		[SerializeField] private RawImage playerOnlyDisplay = default;
 		[SerializeField] private Camera playerOnlyCamera = default;
-		[SerializeField] private float fadeInTime = 2;
+
+		[SerializeField] private Graphic[] fadeInGraphics = default;
+		[SerializeField] private float graphicsFadeInTime = 2;
 		[SerializeField] private ControllerMenuItem startMenuItem = default;
-		[SerializeField] private GameObject[] onFadeFinishEnable = default;
-		[SerializeField] private int gameOverMusicIndex = 2;
 
 		private MusicInstance gameOverMusic = default;
 		private bool fadingIn;
-		private float fadedTime;
+		private float totalFadeTime;
+		private float[] alphaValues;
 
 		private void Start()
 		{
 			gameObject.SetActive(false);
 			(transform as RectTransform).anchoredPosition = Vector2.zero;
 			EventManager.PlayerDiedEvent += Show;
-			for (int i = 0; i < onFadeFinishEnable.Length; i++)
-			{
-				onFadeFinishEnable[i].SetActive(false);
-			}
 			gameOverMusic = new MusicInstance(1, 6, gameOverMusicIndex, 1);
+
+			//Fetch alpha values
+			alphaValues = new float[fadeInGraphics.Length];
+			for (int i = 0; i < fadeInGraphics.Length; i++)
+			{
+				alphaValues[i] = fadeInGraphics[i].color.a;
+				fadeInGraphics[i].color = new Color(fadeInGraphics[i].color.r, fadeInGraphics[i].color.g, fadeInGraphics[i].color.b, 0);
+			}
 		}
 		public void Show(Entities.Entity killer)
 		{
@@ -60,28 +69,33 @@ namespace EoE.UI
 			if (!fadingIn)
 				return;
 
-			if(fadedTime < fadeInTime)
+			totalFadeTime += Time.unscaledDeltaTime;
+			if(totalFadeTime < blackPlaneFadeInTime)
 			{
-				fadedTime += Time.unscaledDeltaTime;
-				if (fadedTime > fadeInTime)
+				if (totalFadeTime > blackPlaneFadeInTime)
 				{
-					fadedTime = fadeInTime;
-					FinishedFading();
+					totalFadeTime = blackPlaneFadeInTime;
 				}
 			}
 
-			float alpha = (fadedTime / fadeInTime) * finalBlackAlpha;
-			blackPlane.color = new Color(0, 0, 0, alpha);
-			playerOnlyDisplay.color = new Color(1, 1, 1, alpha);
+			float blackPlaneAlpha = (totalFadeTime / blackPlaneFadeInTime) * finalBlackAlpha;
+			blackPlane.color = new Color(0, 0, 0, Mathf.Clamp01(blackPlaneAlpha));
+			playerOnlyDisplay.color = new Color(1, 1, 1, Mathf.Clamp01(blackPlaneAlpha));
+
+			float graphicsAlpha = (totalFadeTime - blackPlaneFadeInTime) / graphicsFadeInTime;
+			for(int i = 0; i < fadeInGraphics.Length; i++)
+			{
+				fadeInGraphics[i].color = new Color(fadeInGraphics[i].color.r, fadeInGraphics[i].color.g, fadeInGraphics[i].color.b, alphaValues[i] * Mathf.Clamp01(graphicsAlpha));
+			}
+
+			if(graphicsAlpha >= 1)
+			{
+				FinishedFading();
+			}
 		}
 		private void FinishedFading()
 		{
 			fadingIn = false;
-			for(int i = 0; i < onFadeFinishEnable.Length; i++)
-			{
-				onFadeFinishEnable[i].SetActive(true);
-			}
-
 			startMenuItem.Select();
 		}
 		public void OnRestart()
